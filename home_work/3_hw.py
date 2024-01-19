6d6e2c8ccb7b9337b60a5ae93b8436bb703ef935
Nika <miishka_93@list.ru>
2024-01-19 20:21:28 +0300

    дз 3 целое

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -1,3 +1,4 @@
+# Задача 2
 def max(a, b):
     if a >= b:
         return a
@@ -7,6 +8,7 @@ def max(a, b):
 
 print('Большее число:', max(5 , 7 ))
 
+# Задача 3
 def much(a, b):
     if abs(a-b) > 135:
         print('Yes')
@@ -14,5 +16,47 @@ def much(a, b):
         print('No')
     return()
 
+
 much(22,600)
 
+
+# Задача 4
+def month(a):
+    if a in range(1 , 3) or a == 12:
+        print('Сезон года - зима')
+    elif a in range(3 , 6):
+        print('Сезон года - весна')
+    elif a in range(6 , 9):
+        print('Сезон года - лето')
+    elif a in range(9 , 12):
+        print('Сезон года - осень')
+    else:
+        print('Нет такого месяца, введите нармальна')
+    return()
+
+
+month(1)
+
+# Задача 5
+def ten(a, b, c):
+    if a > 10 and b>10 and c>10:
+        print ('YES')
+    else:
+        print ('No')
+    return ()
+
+ten(5, 10,29)
+
+
+# Задача 6
+def pos(a: list):
+    print (len([x for x in a if x > 0]))
+
+pos([5, 10, -15, -29, -30])
+
+
+# Задача 7
+def days(year: int, month: int):
+    print(((year * 12) + month) * 29)
+
+days(3, 4)
